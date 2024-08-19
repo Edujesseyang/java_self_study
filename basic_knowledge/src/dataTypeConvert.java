a62569231e8bff654f05ec056026e94cd86b4813
Edujesseyang <edujesseyang@gmail.com>
2024-08-18 18:45:34 -0700

    add

--- basic_knowledge/src/dataTypeConvert.java
+++ basic_knowledge/src/dataTypeConvert.java
@@ -11,6 +11,22 @@ public class dataTypeConvert {
         System.out.println("To concat chars in to a string: \nChars: " + a + ", " + b + ", " + c);
         System.out.println("The result string is : " + abc);
 
+        // data type conversion: (new type) newName = (target type) varName
+        int i = 12;
+        byte j = (byte) i;
+        System.out.println(j);
+
+        // If data out of the range for new type. Data overflow happens.
+        int x = 130;
+        byte y = (byte) x;
+        System.out.println(y); // it will print -126
+
+        // If double to int, it will round down to int.
+        double dd = 245.54;
+        int id = (int) dd;
+        System.out.println(id); // it will print 245)
+
+      
     }
 
     public static int double_int(final double input){
