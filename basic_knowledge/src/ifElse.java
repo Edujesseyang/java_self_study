7718780f3e9d1595d235951ec7a93c56fe2fbb01
Edujesseyang <edujesseyang@gmail.com>
2024-08-19 15:22:21 -0700

    add

---
+++ basic_knowledge/src/ifElse.java
@@ -0,0 +1,62 @@
+
+public class ifElse {
+
+    public static void main(String[] args) {
+        // 1. if else example:
+        int a = 12;
+        int b = 10;
+        if (a < b) {
+            System.out.println(a + " is less than " + b);
+        } else {
+            System.out.println(a + " is greater than " + b);
+        }
+
+        // 2. if else-if example:
+        int c = 15;
+        if (a < b) {
+            System.out.println(a + " is less than " + b);
+        } else if (a < c) {
+            System.out.println(a + " is also less than " + c);
+        } else {
+            System.out.println(a + " is not less than " + b + " or " + c);
+        }
+
+        // 3. switch case (old way)
+        switch (a) {
+            case 10:
+                System.out.println("a is 10");
+                break;
+            case 9:
+                System.out.println("a is 9");
+                break;
+            default:
+                System.out.println("a is not 10 or 9");
+                break;
+        }
+        // 4. rule-switch: (better practice)
+        String text = switch (a) {
+            case 10 ->
+                "a is 10";
+            case 9 ->
+                "a is 9";
+            default ->
+                "a is not 10 or 9";
+        };
+        System.out.println(text);
+
+    }
+
+    public static int findMonthDay(int month) {
+        int monthDay = switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 ->
+                31;
+            case 4, 6, 9, 11 ->
+                30;
+            case 2 ->
+                28;
+            default ->
+                -1;
+        };
+        return monthDay;
+    }
+}
