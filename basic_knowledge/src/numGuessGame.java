bbb9c91b1dce930a8674c03677cf442080e9b71b
Edujesseyang <edujesseyang@gmail.com>
2024-08-23 15:20:40 -0700

    add

--- basic_knowledge/src/numGuessGame.java
+++ basic_knowledge/src/numGuessGame.java
@@ -1,4 +1,5 @@
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,46 +8,43 @@ public class numGuessGame {
     public static void main(String[] args) {
 
         int max = getDifficulty();
+
         int targetNum = getRandomNum(max);
         guessNum(targetNum);
 
     }
 
     public static int getRandomNum(int max) {
-        return new Random().nextInt(max + 1);
+        return new Random().nextInt(max) + 1;
     }
 
     public static int getDifficulty() {
-        String choice;
-        int max;
+        System.out.println("What is the difficult level do you want to play? (1: Easy(0-100), 2: Medium(0-500), 3: Hard(0-1000))");
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("What is the difficult level do you want to play? (1: Easy(0-100), 2: Medium(0-500), 3: Hard(0-1000))");
-
             while (true) {
-
-                choice = sc.nextLine();
-
-                switch (choice) {
-                    case "1" -> {
-                        max = 100;
-                        return max;
+                try {
+                    int choice = sc.nextInt();
+                    if (choice == 1 || choice == 2 || choice == 3) {
+                        switch (choice) {
+                            case 1 -> {
+                                return 100;
+                            }
+                            case 2 -> {
+                                return 500;
+                            }
+                            case 3 -> {
+                                return 1000;
+                            }
+                        }
+                    } else {
+                        System.err.println("Enter 1, 2 or 3 only: ");
                     }
-                    case "2" -> {
-                        max = 500;
-                        return max;
-                    }
-                    case "3" -> {
-                        max = 1000;
-                        return max;
-                    }
-                    default ->
-                        System.err.println("Please enter 1, 2, and 3 only! Try again:");
+                } catch (InputMismatchException e) {
+                    System.err.println("Invalid input: " + e.getMessage());
+                    sc.next();
                 }
-
             }
-
         }
-
     }
 
     /**
@@ -56,28 +54,26 @@ public class numGuessGame {
      */
     public static void guessNum(int target) {
         int chance = 7;
-
         try (Scanner sc = new Scanner(System.in)) {
-            String inputStr;
-            int inputNum;
             while (chance > 0) {
                 System.out.println("Guess the number: ");
                 System.out.println("You have " + chance + " chance to guess");
-
-                inputStr = sc.nextLine();  // FIXME
-                inputNum = Integer.parseInt(inputStr);  // FIXME
-                if (inputNum == target) {
-                    System.out.println("Bingo! You win, the number is " + target + ". You used " + (7 - chance) + " times to win! ");
-                    break;
-                } else if (inputNum > target) {
-                    System.out.println("It is too big.");
-                } else {
-                    System.out.println("It is too small.");
+                try {
+                    int inputNum = sc.nextInt();  // FIXME
+                    if (inputNum == target) {
+                        System.out.println("Bingo! You win, the number is " + target + ". You used " + (7 - chance) + " times to win! ");
+                        break;
+                    } else if (inputNum > target) {
+                        System.out.println("It is too big.");
+                    } else {
+                        System.out.println("It is too small.");
+                    }
+                    chance--;
+                } catch (InputMismatchException e) {
+                    System.err.println("Invalid input: " + e.getMessage());
+                    sc.next();
                 }
-
-                chance--;
             }
-
             if (chance == 0) {
                 System.out.println("You lose, the number is " + target + ".");
             }
