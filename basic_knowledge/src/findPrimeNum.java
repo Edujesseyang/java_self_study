60193f16168a9a18a19c0d4d3fe3606a2b687337
Edujesseyang <edujesseyang@gmail.com>
2024-08-22 19:10:12 -0700

    add

--- basic_knowledge/src/findPrimeNum.java
+++ basic_knowledge/src/findPrimeNum.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 public class findPrimeNum {
 
     public static void main(String[] args) {
-        int[] primeNum = findPrime(100);
-        for (int element : primeNum) {
-            System.out.print(element + " ");
-        }
-        System.out.println();
+        int[] primeNum = findPrime(0, 100);
+        printArr(primeNum);
+        int[] randArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+        int[] primeNum2 = oneFuncToDone(randArr);
+        printArr(primeNum2);
 
         int a = 10;
         int b = 5;
@@ -17,9 +17,9 @@ public class findPrimeNum {
 
     }
 
-    public static int[] findPrime(int max) {
+    public static int[] findPrime(int min, int max) {
         ArrayList<Integer> primeNum = new ArrayList<>();
-        for (int i = 2; i <= max; i++) {
+        for (int i = min; i <= max; i++) {
             if (isPrime(i)) {
                 primeNum.add(i);
             }
@@ -38,6 +38,34 @@ public class findPrimeNum {
             }
         }
         return true;
+    }
+
+    public static int[] oneFuncToDone(int[] array) {
+        ArrayList<Integer> primeList = new ArrayList<>();
+        boolean isPrime;
+        for (int element : array) {
+            isPrime = true;
+            if (element <= 1) {
+                isPrime = false;
+            }
+            for (int i = 2; i <= Math.sqrt(element); i++) {
+                if (element % i == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                primeList.add(element);
+            }
+        }
+        int[] primeListArr = primeList.stream().mapToInt(i -> i).toArray();
+        return primeListArr;
+    }
+
+    public static void printArr(int[] array) {
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
     }
 
 }
